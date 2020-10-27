737dce15186a654c82d2ee677745eb7ee2145ea8
Jameel Socorro <jameelsocorro@gmail.com>
2020-10-27 21:15:49 +0800

    home

--- pages/home.js
+++ pages/home.js
@@ -9,6 +9,7 @@ const HomePageStyled = styled.section`
         align-items: center;
         flex-direction: row;
         flex-flow: row-reverse;
+        justify-content: space-between;
     }
 `;
 
@@ -21,34 +22,35 @@ const MokcupImageStyled = styled.img`
     }
 
     @media only screen and (min-device-width: 1024px) {
-        width: 70%;
-    }
-
-    @media only screen and (min-device-width: 1440px) {
-        width: 80%;
+        max-width: 600px;
     }
 `;
 
 const LeftContent = styled.section`
+    max-width: 450px;
+    
     margin-top: ${spacing(3)}; 
     padding: 0 ${spacing(3)};
 `;
 
 const LogoStyled = styled.img`        
     width: 250px;
+
+    @media only screen and (min-device-width: 1024px) {
+        width: 300px;
+    }
 `;
 
 const SloganStyled = styled.label`
     display: block;
-    font-size: 1.3rem;
+    font-size: 1.5rem;
     font-weight: medium;
     text-transform: uppercase;
 
     margin-top: ${spacing(1)}; 
 `;
 
-const DescStyled = styled.div`
-    font-size: 0.8rem;    
+const DescStyled = styled.div`    
     font-weight: 200;
     text-align: justify;
 
