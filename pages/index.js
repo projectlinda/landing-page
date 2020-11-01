ff925d03e7ed59f694968b18b5bf270fd7935096
Jameel Socorro <jameelsocorro@gmail.com>
2020-11-01 19:27:38 +0800

    viewport

--- pages/index.js
+++ pages/index.js
@@ -32,9 +32,9 @@ export default function Home() {
   return (
     <ThemeProvider theme={theme}>
       <div>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <Head>          
           <title>Carinderya</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
